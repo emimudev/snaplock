b7921d8636e9542b38f02b0428db81085d0f3267
Emiliano-Murillo <emilianomurillo12@gmail.com>
2023-02-26 16:09:25 -0600

    Init project

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  jsxBracketSameLine: false
+}
