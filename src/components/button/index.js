67281835df099eb01f4aafc10b82b71035963b2b
Emiliano-Murillo <emilianomurillo12@gmail.com>
2023-02-28 22:34:11 -0600

    Add dark mode to buttons

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -41,11 +41,16 @@ const buttonStyles = tv({
   base: 'relative gap-2 flex overflow-hidden items-center justify-center rounded-md font-medium focus:outline-none active:scale-[0.97] transition-transform focus-visible:ring hover:transition-colors',
   variants: {
     color: {
-      primary: 'bg-gray-900 hover:bg-gray-700 text-white',
-      secondary: 'bg-sky-500 hover:bg-sky-600 text-white',
-      success: 'bg-emerald-500 hover:bg-emerald-600 text-white',
-      warning: 'bg-amber-500 hover:bg-amber-600 text-white',
-      danger: 'bg-red-500 hover:bg-red-600 text-white'
+      primary:
+        'bg-zinc-900 hover:bg-zinc-700 text-white dark:bg-zinc-600 dark:hover:bg-zinc-700',
+      secondary:
+        'bg-sky-500 hover:bg-sky-600 text-white dark:bg-sky-600 dark:hover:bg-sky-700',
+      success:
+        'bg-emerald-500 hover:bg-emerald-600 text-white dark:bg-emerald-600 dark:hover:bg-emerald-700',
+      warning:
+        'bg-amber-500 hover:bg-amber-600 text-white dark:bg-yellow-600 dark:hover:bg-yellow-700',
+      danger:
+        'bg-red-500 hover:bg-red-600 text-white dark:bg-red-600 dark:hover:bg-red-700'
     },
     size: {
       xs: 'px-1.5 h-6 text-xs min-w-[5rem] rounded-[7px]',
@@ -59,13 +64,13 @@ const buttonStyles = tv({
       true: 'rounded-full'
     },
     bordered: {
-      true: 'border-2 bg-transparent'
+      true: 'border-2 bg-transparent dark:bg-transparent'
     },
     flat: {
       true: ''
     },
     ghost: {
-      true: 'bg-transparent'
+      true: 'bg-transparent dark:bg-transparent'
     },
     shadow: {
       true: ''
@@ -75,30 +80,36 @@ const buttonStyles = tv({
     }
   },
   compoundVariants: [
+    // Bordered compound variants
     {
       color: 'primary',
       bordered: true,
-      className: 'text-gray-900 border-gray-900 hover:bg-gray-100'
+      className:
+        'text-zinc-900 border-zinc-900 hover:bg-zinc-100 dark:border-zinc-600 dark:text-zinc-200 dark:hover:bg-zinc-800'
     },
     {
       color: 'secondary',
       bordered: true,
-      className: 'text-sky-500 border-sky-500 hover:bg-sky-100'
+      className:
+        'text-sky-500 border-sky-500 hover:bg-sky-100 dark:border-sky-600 dark:text-sky-300 dark:hover:bg-sky-900/30'
     },
     {
       color: 'success',
       bordered: true,
-      className: 'text-emerald-500 border-emerald-500 hover:bg-emerald-100'
+      className:
+        'text-emerald-500 border-emerald-500 hover:bg-emerald-100 dark:border-emerald-600 dark:text-emerald-300 dark:hover:bg-emerald-900/30'
     },
     {
       color: 'warning',
       bordered: true,
-      className: 'text-amber-500 border-amber-500 hover:bg-amber-100'
+      className:
+        'text-amber-500 border-amber-500 hover:bg-amber-100 dark:border-amber-600 dark:text-amber-400 dark:hover:bg-amber-900/30'
     },
     {
-      color: 'error',
+      color: 'danger',
       bordered: true,
-      className: 'text-red-500 border-red-500 hover:bg-red-100'
+      className:
+        'text-red-500 border-red-500 hover:bg-red-100 dark:border-red-600 dark:text-red-300 dark:hover:bg-red-900/30'
     },
     // Shadow compound variants
     {
@@ -130,53 +141,63 @@ const buttonStyles = tv({
     {
       color: 'primary',
       flat: true,
-      className: 'bg-gray-100 text-gray-600 hover:bg-gray-200'
+      className:
+        'bg-zinc-100 text-zinc-600 hover:bg-zinc-200 dark:bg-white/5 dark:text-zinc-200 dark:hover:bg-white/10'
     },
     {
       color: 'secondary',
       flat: true,
-      className: 'bg-sky-100 text-sky-600 hover:bg-sky-200'
+      className:
+        'bg-sky-100 text-sky-600 hover:bg-sky-200 dark:bg-sky-900/50 dark:hover:bg-sky-800/50 dark:text-sky-300'
     },
     {
       color: 'success',
       flat: true,
-      className: 'bg-emerald-100 text-emerald-600 hover:bg-emerald-200'
+      className:
+        'bg-emerald-100 text-emerald-600 hover:bg-emerald-200 dark:bg-emerald-900/50 dark:hover:bg-emerald-800/50 dark:text-emerald-300'
     },
     {
       color: 'warning',
       flat: true,
-      className: 'bg-amber-100 text-amber-600 hover:bg-amber-200'
+      className:
+        'bg-amber-100 text-amber-600 hover:bg-amber-200 dark:bg-amber-900/50 dark:hover:bg-amber-800/50 dark:text-amber-300'
     },
     {
       color: 'danger',
       flat: true,
-      className: 'bg-red-100 text-red-600 hover:bg-red-200'
+      className:
+        'bg-red-100 text-red-600 hover:bg-red-200 dark:bg-red-900/50 dark:hover:bg-red-800/50 dark:text-red-300'
     },
     // Ghost compound variants
     {
       color: 'primary',
       ghost: true,
-      className: 'text-gray-600 hover:bg-gray-100'
+      className:
+        'text-zinc-600 hover:bg-zinc-100 dark:text-zinc-400 dark:hover:bg-zinc-800'
     },
     {
       color: 'secondary',
       ghost: true,
-      className: ' text-sky-600 hover:bg-sky-100'
+      className:
+        'text-sky-600 hover:bg-sky-100 dark:text-sky-400 dark:hover:bg-sky-900/40'
     },
     {
       color: 'success',
       ghost: true,
-      className: ' text-emerald-600 hover:bg-emerald-100'
+      className:
+        'text-emerald-600 hover:bg-emerald-100 dark:text-emerald-400 dark:hover:bg-emerald-900/40'
     },
     {
       color: 'warning',
       ghost: true,
-      className: ' text-amber-600 hover:bg-amber-100'
+      className:
+        'text-amber-600 hover:bg-amber-100 dark:text-amber-400 dark:hover:bg-amber-900/40'
     },
     {
       color: 'danger',
       ghost: true,
-      className: ' text-red-600 hover:bg-red-100'
+      className:
+        'text-red-600 hover:bg-red-100 dark:text-red-400 dark:hover:bg-red-900/40'
     }
   ],
   defaultVariants: {
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -1,11 +1,10 @@
 import { Button, Layout } from '@/components'
 import { getServerUser } from '@/utils'
-import { signOut } from 'next-auth/react'
 
 export default function Overview({ user }) {
   return (
     <Layout user={user}>
-      <Button
+      {/* <Button
         ghost
         onClick={(e) => {
           e.preventDefault()
@@ -13,8 +12,57 @@ export default function Overview({ user }) {
         }}
       >
         Sign out
-      </Button>
+      </Button> */}
       <Button>Hola mundo</Button>
+      <Button color="secondary">Hola mundo</Button>
+      <Button color="success">Hola mundo</Button>
+      <Button color="warning">Hola mundo</Button>
+      <Button color="danger">Hola mundo</Button>
+      <hr />
+      flat
+      <Button flat>Hola mundo</Button>
+      <Button color="secondary" flat>
+        Hola mundo
+      </Button>
+      <Button color="success" flat>
+        Hola mundo
+      </Button>
+      <Button color="warning" flat>
+        Hola mundo
+      </Button>
+      <Button color="danger" flat>
+        Hola mundo
+      </Button>
+      <hr />
+      bordered
+      <Button bordered>Hola mundo</Button>
+      <Button color="secondary" bordered>
+        Hola mundo
+      </Button>
+      <Button color="success" bordered>
+        Hola mundo
+      </Button>
+      <Button color="warning" bordered>
+        Hola mundo
+      </Button>
+      <Button color="danger" bordered>
+        Hola mundo
+      </Button>
+      <hr />
+      Ghost
+      <Button ghost>Hola mundo</Button>
+      <Button color="secondary" ghost>
+        Hola mundo
+      </Button>
+      <Button color="success" ghost>
+        Hola mundo
+      </Button>
+      <Button color="warning" ghost>
+        Hola mundo
+      </Button>
+      <Button color="danger" ghost>
+        Hola mundo
+      </Button>
     </Layout>
   )
 }
