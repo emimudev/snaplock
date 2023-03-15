11026e2b5631ee7f4346d2426224eebaa6a1837b
Emiliano-Murillo <emilianomurillo12@gmail.com>
2023-03-14 18:32:03 -0600

    Remove all folders correctly

--- src/pages/api/bin/folders/[id].js
+++ src/pages/api/bin/folders/[id].js
@@ -1,10 +1,13 @@
-import { removeFolder } from '@/api-utils/folders'
 import dbConnect from '@/lib/dbConnnect'
+import FolderModel from '@/models/FolderModel'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../../auth/[...nextauth]'
 
 export default async function handler(req, res) {
-  const { method } = req
+  const {
+    query: { id },
+    method
+  } = req
   const session = await getServerSession(req, res, authOptions)
   if (!session) {
     return res.status(401).json({ success: false })
@@ -14,13 +17,30 @@ export default async function handler(req, res) {
 
   switch (method) {
     case 'DELETE':
+      // try {
+      //   const { id } = req.query
+      //   const folder = await removeFolder({ id })
+      //   if (!folder) {
+      //     return res.status(400).json({ success: false })
+      //   }
+      //   res.status(200).json(folder)
+      // } catch (error) {
+      //   console.log({ error })
+      //   res.status(400).json({ success: false })
+      // }
+      // break
       try {
-        const { id } = req.query
-        const folder = await removeFolder({ id })
-        if (!folder) {
-          return res.status(400).json({ success: false })
+        const folderToDelete = await FolderModel.findById(id)
+        if (!folderToDelete) {
+          return res.status(404).json({ success: false })
         }
-        res.status(200).json(folder)
+        const result = await FolderModel.deleteMany({
+          $or: [{ _id: id }, { parentFolders: { $in: [id] } }]
+        })
+        res.status(200).json({
+          folder: folderToDelete,
+          result
+        })
       } catch (error) {
         console.log({ error })
         res.status(400).json({ success: false })
--- src/pages/api/folders/restore/[id].js
+++ src/pages/api/folders/restore/[id].js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
           { isDeleted: false }
         )
         res.status(200).json({
-          folderId: folderToDelete.name,
+          folder: folderToDelete,
           result
         })
       } catch (error) {
